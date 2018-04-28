3e5848a0a799c0d237945fc80c2e00a6b34566eb
cod33 <corinn.durham@pitt.edu>
2018-04-28 09:54:21 -0400

    .dihedrals.py.swp 	pull_struc_nterm.py

--- conc.py
+++ conc.py
@@ -1,13 +1,14 @@
 import numpy as np
 
 #dimer
-moles = 2/6.02E23
+moles = 1/6.02E23
 cub_A = (4/3)*np.pi*(125)**3
+print(cub_A)
 Liters= cub_A * 1E-27
 R = 1.987e-3
 T = 298
 conc = moles/Liters 
-print('dimer',conc)
+print('1 molecules',conc)
 
 #trimer
 moles = 3/6.02E23
@@ -15,5 +16,16 @@ cub_A = (4/3)*np.pi*(125)**3
 Liters= cub_A * 1E-27
 R = 1.987e-3
 T = 298
-conc = moles/Liters 
-print('trimer',conc)
+conc_m = moles/Liters 
+print('3 molecules',conc_m)
+
+C_eff = 0.0078
+
+C_eff_m = C_eff/3
+print('trimer as 1/3 monomer',C_eff_m)
+#as if we multiplied by 1L, divided by simulation volume
+print('volumechange',C_eff_m/Liters)
+C_m = C_eff*6.02E23*Liters
+print(C_m)
+
+print(0.0026*6.02E23)
