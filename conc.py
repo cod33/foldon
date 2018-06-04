6c354493f1012206bf984df9a1a4f759692c44ed
cod33 <corinn.durham@pitt.edu>
2018-06-04 12:35:26 -0400

    maximum atomwise distance:    maxes.py 	stores radius of gyration:    r_gyr.py

--- conc.py
+++ conc.py
@@ -1,14 +1,14 @@
 import numpy as np
 
 #dimer
-moles = 1/6.02E23
+moles = 2/6.02E23
 cub_A = (4/3)*np.pi*(125)**3
 print(cub_A)
 Liters= cub_A * 1E-27
 R = 1.987e-3
 T = 298
 conc = moles/Liters 
-print('1 molecules',conc)
+print('2 molecules',conc)
 
 #trimer
 moles = 3/6.02E23
